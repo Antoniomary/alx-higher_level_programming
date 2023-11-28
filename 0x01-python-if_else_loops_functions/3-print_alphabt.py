97dc0e190ca7a3a3f0b612829021a4c480e7d691
Antoniomary <amanso.totti@yahoo.com>
2023-11-28 03:50:57 -0800

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# use python ascii code numbers for lowercase alphabets
+for ascii in range(97, 123):
+    if chr(ascii) not in 'qe':
+        # print one alphabet at a time and remove auto-newline
+        print(chr(ascii), end='')
