1414462a2ee101624a023e46fae6897669f8b407
Antoniomary <amanso.totti@yahoo.com>
2023-11-28 09:40:41 -0800

    removed 3-print_alphabt.py because of checker problem

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# use python ascii code numbers for lowercase alphabets
-
-for n in range(97, 123):
-    if n != 101 and n != 113:
-        print(chr(n).format(), end='')
-#        print("{:c}".format(n), end='')
