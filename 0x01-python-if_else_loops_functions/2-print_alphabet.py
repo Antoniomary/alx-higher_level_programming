b5ef8f764a892e88204afa45f0d0b4275afd757d
Antoniomary <amanso.totti@yahoo.com>
2023-11-28 03:40:19 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,5 +2,5 @@
 
 # use python ascii code numbers for lowercase alphabets
 for ascii in range(97, 123):
-    # print one alphabet at a time and remove auto-newline
+    # outputs one alphabet at a time and remove auto-newline
     print(chr(ascii), end='')
