28692b0444fcb0f434c30fc3f7ac21c74b4a99c5
Antoniomary <amanso.totti@yahoo.com>
2023-11-30 07:47:33 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,26 +5,27 @@ guillaume@ubuntu:~/0x02$ ./3-infinite_add.py 79 10
 89
 """
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-av = sys.argv
-ac = len(av)
+    av = sys.argv
+    ac = len(av)
 
-if ac == 1:
-    print("0")
-else:
-    for i in range(1, ac):
-        j = 0
-        arg_len = len(av[i])
-        while j < arg_len:
-            if not av[i][j].isdigit():
-                if j != 0 or av[i][j] not in "+-":
-                    print("Error")
-                    sys.exit(1)
-            j += 1
+    if ac == 1:
+        print("0")
+    else:
+        for i in range(1, ac):
+            j = 0
+            arg_len = len(av[i])
+            while j < arg_len:
+                if not av[i][j].isdigit():
+                    if j != 0 or av[i][j] not in "+-":
+                        print("Error")
+                        sys.exit(1)
+                j += 1
 
-    sum = 0
-    for i in range(1, ac):
-        sum += int(av[i])
+        sum = 0
+        for i in range(1, ac):
+            sum += int(av[i])
 
-    print(sum)
+        print(sum)
