68bc5d49abe6f5a33f48e3b95546deaa79df4c5d
Antoniomary <amanso.totti@yahoo.com>
2023-12-06 11:16:20 -0800

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """a function that replaces or adds key/value in a dictionary.
+
+    Args:
+        a_dictionary: the dictionary
+        key: the key
+        value: the value to attach to the key
+    """
+    if a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
