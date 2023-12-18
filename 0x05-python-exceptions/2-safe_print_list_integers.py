31e02e3dac6bbf348e09bec11899cdcd16e00a2e
Antoniomary <amanso.totti@yahoo.com>
2023-12-18 12:24:47 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        i = 0
-        count = 0
-        while i < x:
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            i += 1
-    except IndexError:
-        print("IndexError: list index out of range", end="")
-    finally:
-        print()
-        return count
+    i, count = 0, 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        i += 1
+    print()
+    return count
