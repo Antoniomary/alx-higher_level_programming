c61842fb9867363e6db742e29e5813c477b50d20
Antoniomary <amanso.totti@yahoo.com>
2023-12-19 08:02:37 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a class definition named Square"""
+
+
+class Square:
+    """a class that defines a square by a private instance attribute: size"""
+
+    def __init__(self, size):
+        self.__size = size
