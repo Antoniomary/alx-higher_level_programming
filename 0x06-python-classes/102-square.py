09f6b4b1c77ee9b25d824007cc3517c1680571a8
Antoniomary <amanso.totti@yahoo.com>
2023-12-19 20:54:49 -0800

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""a class definition named Square"""
+
+
+class Square:
+    """a class that defines a square by a private instance
+    attribute: size. The size must be of type int and >= 0
+    """
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """a public instance method that returns the area of square"""
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """it retrieves the value of the private attribute: size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """it sets the value of size"""
+        if isinstance(value, int):
+            if (value < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def __eq__(self, other):
+        """compares for equality"""
+
+        if isinstance(other, Square):
+            return self.__size == other.size
+        return False
+
+    def __lt__(self, other):
+        """does the less than operator"""
+
+        if isinstance(other, Square):
+            return self.__size < other.size
+        return False
+
+    def __gt__(self, other):
+        """does the greater than operator"""
+
+        if isinstance(other, Square):
+            return self.__size > other.size
+        return False
+
+    def __le__(self, other):
+        """does the less than or equal to operator"""
+
+        if isinstance(other, Square):
+            return self.__size <= other.size
+        return False
+
+    def __ge__(self, other):
+        """does the greater than or equal to operator"""
+
+        if isinstance(other, Square):
+            return self.__size >= other.size
+        return False
