3ba6022a3ac7252abee4e3611fe33299de9c2a24
Antoniomary <amanso.totti@yahoo.com>
2024-01-08 15:04:59 -0800

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    A module for a Square class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        A class that defines a Square shape, subclass Rectangle
+    """
+    def __init__(self, size):
+        """creates an instance of Square"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns the area of a square"""
+        return Rectangle.area(self)
+
+    def __str__(self):
+        """returns square description"""
+        description = Rectangle.__str__(self)
+        return description.replace("Rectangle", "Square")
