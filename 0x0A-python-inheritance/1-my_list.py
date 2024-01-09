47f87b7d903d6a3c9cb01d1e1b0191e6ced52bb8
Antoniomary <amanso.totti@yahoo.com>
2024-01-08 16:27:46 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,9 @@
 
 class MyList(list):
     """a subclass of list"""
+    def __init__(self):
+        """creates an instance"""
+        super().__init__(self)
 
     def print_sorted(self):
         """prints a list sorted in ascending order"""
