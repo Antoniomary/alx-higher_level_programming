26fd6dd9222b9c5ff35b29ef1271ee311f912968
Antoniomary <amanso.totti@yahoo.com>
2024-03-11 20:03:26 -0700

    13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * adds 2 integers.
+ * @param {first number} a
+ * @param {second number} b
+ * @return {number}
+ */
+function add (a, b) {
+  return a + b;
+}
+
+exports.add = add;
