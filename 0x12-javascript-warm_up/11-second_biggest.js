0448b4f289f07f3d33e83f48719c915916dcc9c9
Antoniomary <amanso.totti@yahoo.com>
2024-03-11 23:26:38 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,10 @@
 #!/usr/bin/node
 process.argv = process.argv.slice(2);
-let ac = process.argv.length;
-let max = 0;
+const ac = process.argv.length;
 let secondMax = 0;
 if (ac > 1) {
-  for (let i = 0; i < ac;) {
-    process.argv[i] = Number(process.argv[i]);
-    if (String(process.argv[i]) === 'NaN') {
-      process.argv[i] = process.argv[ac - 1];
-      ac -= 1;
-    } else {
-      ++i;
-    }
-  }
-  max = process.argv[0];                                                secondMax = max;
-  for (let i = 0; i < ac; ++i) {
-    if (String(secondMax) === 'NaN') {                                      secondMax = process.argv[i];
-    }                                                                     if (process.argv[i] > max) {
-      secondMax = max;
-      max = process.argv[i];
-    } else if (process.argv[i] < max) {
-      secondMax = process.argv[i];
-    }
-  }
-  if (ac <= 1) {
-    secondMax = NaN;
-  }
+  process.argv = process.argv.map(Number);
+  process.argv.sort();
+  secondMax = process.argv[ac - 2];
 }
 console.log(secondMax);
