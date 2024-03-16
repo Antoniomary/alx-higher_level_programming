284093ea7c63057718b2d53ac8d36a5920942845
Antoniomary <amanso.totti@yahoo.com>
2024-03-16 01:30:52 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    a script takes in an argument and displays all values
+    in the states table of hbtn_0e_0_usa where name
+    matches the argument
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    av = argv
+    if len(av) == 5:
+        user = argv[1]
+        passwd = argv[2]
+        db_name = argv[3]
+        condition = MySQLdb.string_literal(argv[4]).decode()
+        print(condition)
+
+        try:
+            db = MySQLdb.connect(
+                    host='localhost', port=3306,
+                    user=user, passwd=passwd, db=db_name
+                )
+            cur = db.cursor()
+            cur.execute(
+                    "SELECT * FROM states WHERE BINARY name = {} \
+                    ORDER BY id ASC".format(condition))
+            for each in cur.fetchall():
+                print(each)
+            cur.close()
+            db.close()
+        except MySQL.error:
+            print("Connection failed")
+    else:
+        print("Usage: <py script> <user> <password> <db>")
