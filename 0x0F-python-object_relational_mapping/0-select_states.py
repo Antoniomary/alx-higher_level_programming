71c2874f2e2372c5373dba47158069c008d3d7ec
Antoniomary <amanso.totti@yahoo.com>
2024-03-16 05:56:56 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
                 print(each)
             cur.close()
             db.close()
-        except MySQL.Error:
-            print("Connection failed")
+        except MySQL.Error as e:
+            print(f"Error: {e}")
     else:
         print("Usage: <py script> <user> <password> <db>")
