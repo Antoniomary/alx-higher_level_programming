894729b86ee13e67b3b87ef73dd71c4dabc9f0a8
Antoniomary <amanso.totti@yahoo.com>
2024-03-27 07:00:32 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL, sends a request to it, and displays the size of the body of the response
-curl -sI -P 5000 "$(cut -d ':' -f1 <<< "$1")" | grep 'Content-Length' | cut -d ' ' -f2
+curl -s -P 5000 "$(cut -d ':' -f1 <<< "$1")" | wc -c 
