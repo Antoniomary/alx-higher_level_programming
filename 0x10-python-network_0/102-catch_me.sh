e320c9465069647214eb8b2d0c628a2a9e3f5cf0
Antoniomary <amanso.totti@yahoo.com>
2024-03-28 04:34:34 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response. 
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "You got Me!" | head -n 11 | tail -n 1 | cut -c 42-52
+curl -sLX PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me_2
