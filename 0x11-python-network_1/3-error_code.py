c84bd1275bb9bfb4fff0514a870c075b40a848bb
Antoniomary <amanso.totti@yahoo.com>
2024-03-28 21:53:18 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    takes in a URL, sends a request to the URL and
+    displays the body of the response (decoded in utf-8).
+"""
+import urllib.request as request
+import urllib.error as error
+import sys
+
+
+def get_response():
+    """gets response from a url and handles error"""
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+        response = res.read()
+        print(response.decode())
+    except error.HTTPError as e:
+        print("Error code:", e.code)
+
+
+if __name__ == "__main__":
+    get_response()
