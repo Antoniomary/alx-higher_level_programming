5a61471c630055cb54b29c468516b85c428f9677
Antoniomary <amanso.totti@yahoo.com>
2024-03-28 21:52:46 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as request
+
+
+def get_stat():
+    """fetches https://alx-intranet.hbtn.io/status and prints it"""
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        response = res.read()
+        print("Body response:")
+        print("    - type:", type(response))
+        print("    - content:", response)
+        print("    - utf8 content:", response.decode())
+
+
+if __name__ == "__main__":
+    get_stat()
