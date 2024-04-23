663b53ae4095617714bedf1e95b085f8c697395f
Antoniomary <amanso.totti@yahoo.com>
2024-04-23 07:07:47 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,16 +2,15 @@
 
 const fs = require('fs');
 const ac = process.argv.length;
-if (ac === 3)
-{
-	try {
-		console.log(fs.readFileSync(process.argv[2], 'utf-8'));
-	} catch (e) {
-		console.log(`{ Error: ${e.message}`);
-		console.log('    at Error (native)');
-		console.log(`  errno: ${e.errno},`);
-		console.log(`  code: '${e.code}',`);
-		console.log(`  syscall: '${e.syscall}',`);
-		console.log(`  path: '${e.path}' }`);
-	}
+if (ac === 3) {
+  try {
+    console.log(fs.readFileSync(process.argv[2], 'utf-8'));
+  } catch (e) {
+    console.log(`{ Error: ${e.message}`);
+    console.log('    at Error (native)');
+    console.log(`  errno: ${e.errno},`);
+    console.log(`  code: '${e.code}',`);
+    console.log(`  syscall: '${e.syscall}',`);
+    console.log(`  path: '${e.path}' }`);
+  }
 }
