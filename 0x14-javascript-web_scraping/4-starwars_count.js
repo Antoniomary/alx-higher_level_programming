44a3a73f29923562c0d5727db65fe087bc98d67b
Antoniomary <amanso.totti@yahoo.com>
2024-04-23 09:02:22 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const ac = process.argv.length;
+if (ac === 3) {
+  const request = require('request');
+  const api = process.argv[2];
+  request({ url: api, json: true }, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    let count = 0;
+    const key = 'https://swapi-api.alx-tools.com/api/people/18/';
+    for (let i = 0; i < body.count; ++i) {
+      if (body.results[i].characters.includes(key)) {
+        count++;
+      }
+    }
+    console.log(count);
+  });
+}
