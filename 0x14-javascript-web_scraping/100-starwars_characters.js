48ebc0bbac95d4197d865c69e893fc6825016641
Antoniomary <amanso.totti@yahoo.com>
2024-06-04 08:56:28 -0700

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const ac = process.argv.length;
+if (ac === 3) {
+  const request = require('request');
+  const api = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+  request({ url: api, json: true }, (error, response, body) => {
+    if (error) {
+      return;
+    }
+    console.log(body.characters);
+    for (const person of body.characters) {
+      request({ url: person, json: true }, (error, response, body1) => {
+        if (error) console.error('Error');
+        else console.log(body1.name);
+      });
+    }
+  });
+}
