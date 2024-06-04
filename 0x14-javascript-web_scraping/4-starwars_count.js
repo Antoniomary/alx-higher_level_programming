4a57beb32dc0b870a01b77fc3372ef380ede74cf
Antoniomary <amanso.totti@yahoo.com>
2024-06-04 08:34:49 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,16 +5,15 @@ if (ac === 3) {
   const request = require('request');
   const api = process.argv[2];
   request({ url: api, json: true }, (error, response, body) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (response && response.statusCode === 200) {
+    if (error) console.error(`Error: ${error.message}`);
+    else {
       let count = 0;
-      const key = 'https://swapi-api.alx-tools.com/api/people/18/';
-      for (let i = 0; i < body['results'].length; ++i) {
-        if (body['results'][i].characters.includes(key)) {
-          count++;
+      const id = '18';
+      for (let i = 0; i < body.results.length; ++i) {
+        for (const person of body.results[i].characters) {
+          if (person.includes(id)) {
+            count++;
+          }
         }
       }
       console.log(count);
